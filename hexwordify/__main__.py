a668194349e739203a6739c1539b1c17ec13ce7c
Dimitris Zervas <dzervas@dzervas.gr>
2018-02-03 09:59:05 +0200

    Add file cli argument

--- hexwordify/__main__.py
+++ hexwordify/__main__.py
@@ -23,15 +23,29 @@ def main():
                             (only used if no hex arguments are given)")
     parse.add_argument("strings", metavar="hex_string", nargs="*",
                        help="Hex strings to turn into words")
+    parse.add_argument("-f", "--file", metavar="FILE", action="append",
+                       help="File to read")
     args = parse.parse_args()
 
-    if len(args.strings) < 1:
+    if not args.strings and args.file is None:
         args.strings = sys.stdin.read().split("\n")
+    elif args.strings:
         args.min_size = 0
 
-    print("\n\r".join(finder(args.strings, min_size=args.min_size,
+    if args.file:
+        args.strings = []
+        for filep in args.file:
+            with open(filep) as fileo:
+                args.strings.append(fileo.read())
+
+    result = finder(args.strings, min_size=args.min_size,
                     replace=not args.no_replace, prepend=args.prepend,
-                    color=not args.no_color)))
+                    color=not args.no_color)
+
+    if isinstance(result, list):
+        print("\n".join(result).strip())
+    else:
+        print(result)
 
 
 if __name__ == "__main__":
